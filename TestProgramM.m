7fa69de215fe83afd5aae5bb4dd2a38a435006b9
bnelson2015 <72219921+bnelson2015@users.noreply.github.com>
2021-02-24 18:28:50 -0700

    Added functionality
    Added functionality to have multiple classes per video frame

--- TestProgramM.m
+++ TestProgramM.m
@@ -1,7 +1,7 @@
 % Suppress all warnings
 warning('off','all');
 
-buoyCount = 0; gateCount = 0; binsCount = 0; torpedoCount = 0;
+greenBuoyCount = 0; gateCount = 0; binCount = 0; torpedoCount = 0; yellowBuoyCount = 0; redBuoyCount = 0
 
 dataStoreDir = "Data";
 dataStoreInfo = dir(fullfile(dataStoreDir));
@@ -12,7 +12,7 @@ for i = 3:size(dataStoreInfo, 1)
     strcmp(dataFolderName, strcat(dataStoreInfo(i-1, 1).name, "_ImageData"));
     
     if (strcmp(dataFolderName, strcat(dataStoreInfo(i-1, 1).name, "_ImageData")))
-        "Image Data Folder Already Exists";
+        "Image Data Folder Already Exists"
     else
         
         newDataDir = fullfile(dataStoreDir, strcat(dataFolderName, "_ImageData"));
@@ -36,72 +36,90 @@ for i = 3:size(dataStoreInfo, 1)
             vidWidth = vid.Width;
             vidHeight = vid.Height;
             
-            % TODO
-            %
-            % Write code to check and make sure that labels exist for a
-            % given video file. 
-            % Ex. If bouy1.avi has no corresponding bouy1_labels.mat file
-            % skip to next video in list
-            %
+            if (j > size(labelList, 2))
+                break
+            end
             
-            
-            % For now, this only works on videos with one class per image
             labels = load(fullfile(dataStoreDir, dataFolderName, labelList{1, j}));
             imageName = convertCharsToStrings(labels.gTruth.LabelDefinitions.Name{1});
             fileNameToStore = fullfile(newDataDir);
             
             for k = 1:numFrames
-             
-                boundingBoxes = labels.gTruth.LabelData{k, imageName};
                 
-                frame = read(vid, k);
-                imgFilename = fullfile(fileNameToStore, strcat(vidName, "frame", num2str(k), "_", "img.jpg"));
-                imwrite(frame, imgFilename);
-                
-                textFilename = fullfile(fileNameToStore, strcat(vidName, "frame", num2str(k), "_", "img.txt"));
-                fileID = fopen(textFilename, "w");
-                
-                boundingBoxes = boundingBoxes{1, 1};
-                
-                for z = 1:size(boundingBoxes, 1)
-                    
-                    currentBoundingBox = boundingBoxes(z, :);
-                    
-                    if isempty(currentBoundingBox)
-                       continue   
-                    end
-               
-                    if strcmp(imageName, "Buoy")
-                        label = "1";
-                        buoyCount = buoyCount+1;
-                    elseif (strcmp(imageName, "Gate"))
-                        label = "2";
-                        gateCount = gateCount+1;
-                    end
-                       
-                    xLeftCorner = currentBoundingBox(1);
-                    yLeftCorner = currentBoundingBox(2);
-                    bBoxWidth = currentBoundingBox(3);
-                    bBoxHeight = currentBoundingBox(4);
-                    
-                    xCenterRatio = (xLeftCorner + (bBoxWidth/2))/vidWidth;
-                    yCenterRatio = (yLeftCorner + (bBoxHeight/2))/vidHeight;
-                    bBoxWidthRatio = bBoxWidth/vidWidth;
-                    bBoxHeightRatio = bBoxHeight/vidHeight;
-        
-                    dataString = strcat(label," ", num2str(xCenterRatio), " ", ...
-                        num2str(yCenterRatio), " ", num2str(bBoxWidthRatio), " ", ...
-                        num2str(bBoxHeightRatio), "\n");
-                    
-                    fprintf(fileID, dataString);
-                   
+                if (k > height(labels.gTruth.LabelData)) 
+                  break
                 end
                 
-                 fclose(fileID);
+                fileID = fopen(textFilename, "w");
+                
+                for numLabels = 1 : size(labels.gTruth.LabelDefinitions, 1)
+                    
+                    labelNames = labels.gTruth.LabelDefinitions.Name;
+                    
+                    labelName = labelNames{numLabels};
+             
+                    boundingBoxes = labels.gTruth.LabelData{k, labelName};
+                    
+                    frame = read(vid, k);
+                    imgFilename = fullfile(fileNameToStore, strcat(vidName, "frame", num2str(k), "_", "img.jpg"));
+                    imwrite(frame, imgFilename);
+                    
+                    textFilename = fullfile(fileNameToStore, strcat(vidName, "frame", num2str(k), "_", "img.txt"));
+                    
+                    boundingBoxes = boundingBoxes{1, 1};
+                    
+                    for z = 1:size(boundingBoxes, 1)
+                        
+                            currentBoundingBox = boundingBoxes(z, :);
+                            
+                            if isempty(currentBoundingBox)
+                               continue   
+                            end
+                       
+                            if strcmp(labelName, "Red_Buoy")
+                                label = "1";
+                                redBuoyCount = redBuoyCount+1;
+                            elseif (strcmp(labelName, "Gate"))
+                                label = "2";
+                                gateCount = gateCount+1;
+                            elseif (strcmp(labelName, "Torpedo"))
+                                label = "3";
+                                torpedoCount = torpedoCount+1;
+                            elseif (strcmp(labelName, "Bin"))
+                                label = "4";
+                                binCount = binCount+1;
+                            elseif (strcmp(labelName, "Green_Buoy"))
+                                label = "5";
+                                greenBuoyCount = greenBuoyCount+1; 
+                            elseif (strcmp(labelName, "Yellow_Buoy"))
+                                label = "6";
+                                yellowBuoyCount = yellowBuoyCount+1;                                 
+                            end
+                               
+                            xLeftCorner = currentBoundingBox(1);
+                            yLeftCorner = currentBoundingBox(2);
+                            bBoxWidth = currentBoundingBox(3);
+                            bBoxHeight = currentBoundingBox(4);
+                            
+                            xCenterRatio = (xLeftCorner + (bBoxWidth/2))/vidWidth;
+                            yCenterRatio = (yLeftCorner + (bBoxHeight/2))/vidHeight;
+                            bBoxWidthRatio = bBoxWidth/vidWidth;
+                            bBoxHeightRatio = bBoxHeight/vidHeight;
+                
+                            dataString = strcat(label," ", num2str(xCenterRatio), " ", ...
+                                num2str(yCenterRatio), " ", num2str(bBoxWidthRatio), " ", ...
+                                num2str(bBoxHeightRatio), "\n");
+                            
+                            fprintf(fileID, dataString);
+                            
+                    end
+                    
+                end
+                
+                fclose(fileID);
                  
             end  
         end
     end
 end
-
-buoyCount, gateCount
+redBuoyCount, gateCount, torpedoCount, binCount, greenBuoyCount, yellowBuoyCount
